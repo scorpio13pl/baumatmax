f8ec8069981b72ea61a1f852c8b52f740b3a2d59
Sebastian Skora <sebastian.skora27@gmail.com>
2020-04-08 21:50:48 +0200

    fixed bgc and navbar

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const hamburger = document.querySelector(".hamburger");
 const navMobile = document.querySelector("nav");
 const mainPage = document.querySelector(".main_page");
 const navLi = document.querySelector(".nav__list");
-
+const header = document.querySelector("header");
 const $navMobile = $("nav");
 hamburger.addEventListener("click", () => {
   console.log("it works");
   hamburger.classList.toggle("is-active");
+  header.classList.toggle("hamburger_open");
   navLi.classList.toggle("open");
   navMobile.classList.toggle("open");
   mainPage.classList.toggle("hamburger_open");
