3a73c986a1c34ba7cc136457135cbc3cf1975a9d
Sebastian Skora <sebastian.skora27@gmail.com>
2020-04-08 19:14:20 +0200

    prefixes added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMobile = document.querySelector("nav");
 const mainPage = document.querySelector(".main_page");
+const navLi = document.querySelector(".nav__list");
 const $navMobile = $("nav");
 hamburger.addEventListener("click", () => {
   console.log("it works");
+  navLi.classList.toggle("open");
   navMobile.classList.toggle("open");
   mainPage.classList.toggle("hamburger_open");
 });
