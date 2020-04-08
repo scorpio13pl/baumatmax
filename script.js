0d5eab8c745d58d2152ae069726efcd127194a37
Sebastian Skora <sebastian.skora27@gmail.com>
2020-04-08 21:33:17 +0200

    added logo new hamburger h1 textes

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const hamburger = document.querySelector(".hamburger");
 const navMobile = document.querySelector("nav");
 const mainPage = document.querySelector(".main_page");
 const navLi = document.querySelector(".nav__list");
+
 const $navMobile = $("nav");
 hamburger.addEventListener("click", () => {
   console.log("it works");
+  hamburger.classList.toggle("is-active");
   navLi.classList.toggle("open");
   navMobile.classList.toggle("open");
   mainPage.classList.toggle("hamburger_open");
